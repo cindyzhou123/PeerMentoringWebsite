aaf3cba7e0737b34aa89f9b1e53712b826c2b5f5
Cindy Zhou <cindy.zhou_work@hotmail.com>
2019-05-14 12:43:05 +1000

    made mentee class

---
+++ src/mentee.py
@@ -0,0 +1,13 @@
+class Mentee(object):
+    def __init__(self,zID, group):
+        self._zID = zID
+        self._mentor = group
+    
+    @property
+    def zID(self):
+        return self._zID
+    
+    @property
+    def group(self):
+        retuen self._group
+    
