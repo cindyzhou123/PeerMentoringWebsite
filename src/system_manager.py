9133f80311880142912a6b491dd22d1b9cddfcf2
Cindy Zhou <z5209956@ad.unsw.edu.au>
2019-05-16 11:33:44 +1000

    system manager

---
+++ src/system_manager.py
@@ -0,0 +1,26 @@
+class SystemManager(object):
+    def __init__(self):
+        self._groups = []
+        self._mentees = []
+        self._mentors = []
+
+    @property
+    def groups(self):
+        return self._groups
+    
+    @property
+    def mentees(self):
+        return self._mentees
+
+    @property
+    def mentors(self):
+        return self._mentors
+    
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def add_mentee(self, mentee):
+        self.mentees.append(mentee)
+    
+    def add_mentor(self, mentor):
+        self.mentors.append(mentor)
