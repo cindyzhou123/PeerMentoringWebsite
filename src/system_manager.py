95dbf1908fceae42013d99f373dc851eb529bba3
Cindy Zhou <z5209956@ad.unsw.edu.au>
2019-05-16 18:11:38 +1000

    add system functionality

--- src/system_manager.py
+++ src/system_manager.py
@@ -25,16 +25,38 @@ class SystemManager(object):
     def add_mentor(self, mentor):
         self.mentors.append(mentor)
     
-    def get_mentor(self, zID):
+    def get_user_by_ID(self, zID):
         for mentor in self.mentors:
             if(mentor.zID == zID):
                 return mentor
+            
+        for mentee in self.mentees:
+            if(mentee.zID == zID):
+                return mentee
                 
         return NULL
     
-    def get_group(self, groupID):
+    def get_group_by_ID(self, groupID):
         for group in self.groups:
             if(group.groupID == groupID):
+                return group
+        
+        return NULL
+    
+    def get_user_by_name(self, name):
+        for mentor in self.mentors:
+            if(mentor.name == name):
+                return mentor
+            
+        for mentee in self.mentees:
+            if(mentee.name == name):
+                return mentee
+                
+        return NULL
+    
+    def get_group_by_name(self, name):
+        for group in self.groups:
+            if(group.name == name):
                 return group
         
         return NULL
