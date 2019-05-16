78cfae1c85b04416bbf25dd4ba9fbb7abdb7d674
Cindy Zhou <z5209956@ad.unsw.edu.au>
2019-05-16 16:52:43 +1000

    tests

--- src/test/test01.py
+++ src/test/test01.py
@@ -1,6 +1,24 @@
+from system_manager import SystemManager
+from users import Users, Mentee, Mentor
+from group import Group
 import pytest
 
-def test_add_mentee():
+@pytest.fixture
+def system():
+    system = SystemManager()
+    return system
+
+def test_create_mentee():
+    print("Creating mentees...\n")
+    mentee1 = Mentee(12345)
+    print(mentee1)
+    mentee2 = Mentee(23456)
+    print(mentee2)
+    
+    assert(mentee1.tag == 2)
+    assert(mentee2.tag == 2)
+    assert(mentee1.zID == 12345)
+    assert(mentee2.zID == 23456)
 
 def test_add_mentor():
 
